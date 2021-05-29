a393e5b72708eac0da52fc55ac6125b8c88efe48
Luburix88 <f.luburic@gmail.com>
2021-05-29 17:10:34 +0200

    ff

--- Vezba JS challange 1/challenge.js
+++ Vezba JS challange 1/challenge.js
@@ -100,10 +100,9 @@ function finalMessage([yourScore, computerScore]) {
   
     //Challenge 4: Change The Color of All Buttons
     var all_buttons = document.getElementsByTagName('button');
-    console.log(all_buttons);
-
+    
     var copyAllButtons = []; 
     for (let i=0; i < all_buttons.length; i++) {
       copyAllButtons.push(all_buttons[i]);
     }
-    console.log(copyAllButtons);
+    
