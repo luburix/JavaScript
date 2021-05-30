4c581a440d13ec899d38b446cf0a091d6f921d18
Luburix88 <f.luburic@gmail.com>
2021-05-30 12:07:17 +0200

    Challenge 4 Done

--- Vezba JS challange 1/challenge.js
+++ Vezba JS challange 1/challenge.js
@@ -114,10 +114,40 @@ function finalMessage([yourScore, computerScore]) {
     } else if (buttonThingy.value === 'green') {
       buttonsGreen();
     } else if (buttonThingy.value === 'reset') {
-      buttonsReset();
+      buttonsColorReset();
     } else if (buttonThingy.value === 'random') {
-        buttonsRandom();
+      buttonsRandom();
     }
   }
 
+  function buttonsRed()  {
+    for (let i=0; i < all_buttons.length; i++)  {
+      all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+      all_buttons[i].classList.add('btn-danger');
+    }
+  }
+
+  function buttonsGreen()  {
+    for (let i=0; i < all_buttons.length; i++)  {
+      all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+      all_buttons[i].classList.add('btn-success');
+    }
+  }
  
+  function buttonsColorReset()  {
+    for (let i=0; i < all_buttons.length; i++)  {
+      all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+      all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+  }
+
+  function buttonsRandom() {
+    var choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning'] 
+  
+
+    for (let i=0; i < all_buttons.length; i++) {
+      var randomNumber = Math.floor(Math.random() * 4);
+      all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+      all_buttons[i].classList.add(choices[randomNumber]);
+    }
+  }
